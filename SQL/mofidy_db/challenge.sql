78d4953b784730fad1a3eeef06ab0856ccbafa13
Romain Vandemaele <romain.v@live.be>
2024-02-03 14:47:23 +0100

    challenge clothing alterations

--- SQL/mofidy_db/challenge.sql
+++ SQL/mofidy_db/challenge.sql
@@ -13,6 +13,28 @@ SELECT * FROM documents;
 
 --challenge clothing alterations
 
+CREATE TABLE clothes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT,
+    design TEXT);
+    
+INSERT INTO clothes (type, design)
+    VALUES ("dress", "pink polka dots");
+INSERT INTO clothes (type, design)
+    VALUES ("pants", "rainbow tie-dye");
+INSERT INTO clothes (type, design)
+    VALUES ("blazer", "black sequin");
+
+ALTER TABLE clothes ADD price INTEGER;
+SELECT * FROM clothes;
+
+UPDATE clothes SET price = id*10;
+SELECT * FROM clothes;
+
+INSERT INTO clothes(type,design,price) VALUES ("shirt","plain brown",23);
+SELECT * FROM clothes;
+
 
 
 --project App impersonator
+
